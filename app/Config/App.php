c276ea48cf8398da2cb5822aa9fb75f1805e4ef3
fran0215 <ortegafranciscoj14@gmail.com>
2025-08-13 15:31:33 -0500

    baseurl

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      * E.g., http://example.com/
      */
-    public string $baseURL = 'http://localhost/gps/public/';
+    public string $baseURL = 'https://apicredit-production.up.railway.app/public/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
